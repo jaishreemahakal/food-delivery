fa55539331c580fc57c5ab63f02e64969dade5b8
Dharmik32 <dharmikchavda07@gmail.com>
2024-11-23 13:40:26 +0530

    url changes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
-const port = 4000;
+const port = "https://food-delivery-backend-8wio.onrender.com";
 
 // middleware
 app.use(express.json());
